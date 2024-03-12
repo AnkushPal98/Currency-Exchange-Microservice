de3a54c878549d01ddbc5d09993f1b6350417285
Ankush <50248359+AnkushPal98@users.noreply.github.com>
2024-03-12 19:10:32 +0530

    Update data.sql

--- currency-exchange-microservice/src/main/resources/data.sql
+++ currency-exchange-microservice/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple)
-values(10001,'USD','INR',82);
+values(10001,'USD','INR',65);
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple)
-values(10002,'EUR','INR',90);
+values(10002,'EUR','INR',75);
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple)
-values(10003,'AUD','INR',54);
+values(10003,'AUD','INR',25);
